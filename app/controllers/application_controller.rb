bb12b4d056e2384658be1a84dec822d030987eca
Aniela M Wolkonowski <anielamw@gmail.com>
2017-04-03 21:02:38 -0400

    refactor per Nates notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
  before_filter :configure_permitted_parameters, if: :devise_controller?
 
+ def admin_user?
+   current_user && current_user.admin == true
+ end
+
  protected
 
  def configure_permitted_parameters
