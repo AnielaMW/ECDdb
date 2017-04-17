12770231754de4de09e417f02e74225387f7372f
Aniela M Wolkonowski <anielamw@gmail.com>
2017-04-17 16:12:25 -0400

    add more information to dance#index

--- spec/features/dances/1_user_views_index_spec.rb
+++ spec/features/dances/1_user_views_index_spec.rb
@@ -22,11 +22,13 @@ feature 'view dances#index', %{
 
     expect(dances[0]).to have_content(dance2.title)
     expect(dances[0]).to have_content(dance2.user.first_name)
+    expect(dances[0]).not_to have_content("Authored by: ")
     expect(dances[1]).to have_content(dance3.title)
-    expect(dances[0]).to have_content(dance3.user.first_name)
-    expect(dances[0]).to have_content(dance3.author)
-    expect(dances[0]).to have_content(dance3.year)
+    expect(dances[1]).to have_content(dance3.user.first_name)
+    expect(dances[1]).to have_content(dance3.author)
+    expect(dances[1]).to have_content(dance3.year)
     expect(dances[2]).to have_content(dance1.title)
-    expect(dances[0]).to have_content(dance1.user.first_name)
+    expect(dances[2]).to have_content(dance1.user.first_name)
+    expect(dances[2]).not_to have_content("Authored by: ")
   end
 end
