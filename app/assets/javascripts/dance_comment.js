66748de95a5ff6c35782bb2edc813d7ec8587c02
Aniela M Wolkonowski <anielamw@gmail.com>
2017-04-26 14:55:48 -0400

    add some minor ids

---
+++ app/assets/javascripts/dance_comment.js
@@ -0,0 +1,7 @@
+// 1. define the correct page or section to best define the submit button (partial)
+// 2. eventListener for the click or submit of the form
+// 3. retreve the information from the form and other params information (comment, comment_type_id, current_user.id, dance.id)
+// 4. prepend the new comment onto the list of comments utilizing the existing format and structure
+// 5. post the new comment to the database.
+
+let findIt = document.getElementById('dance-show');
