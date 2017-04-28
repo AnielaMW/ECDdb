69917701d82607da077a2f17047c6983e459b542
Aniela M Wolkonowski <anielamw@gmail.com>
2017-04-28 12:54:05 -0400

    add javascript to select button, event on click, and get data for the dance#show comments form

--- app/assets/javascripts/dance_comment.js
+++ app/assets/javascripts/dance_comment.js
@@ -1,13 +1,22 @@
-// 1. define the correct page or section to best define the submit button (partial)
-// 2. eventListener for the click or submit of the form
-// 3. retreve the information from the form and other params information (comment, comment_type_id, current_user.id, dance.id)
 // 4. prepend the new comment onto the list of comments utilizing the existing format and structure
 // 5. post the new comment to the database.
 
+submitClick = (event) => {
+  event.preventDefault();
+
+  let newCom = [];
+  newCom.type = $('#dance_comment_comment_type_id').val();
+  newCom.comment = $('#dance_comment_comment').val();
+  newCom.userId = $('#dance_comment_user_id').val();
+  newCom.danceId = $('#dance_comment_dance_id').val();
+
+  let commentList = $('#dance-comment-list>li');
+
+  console.log(commentList);
+};
+
 $(document).ready(() => {
-  let sumbitClick = (event) => {
-  };
-  $('#dance-show #actions');
+  $('#dance-show #actions').on('click', submitClick);
 });
 
 
