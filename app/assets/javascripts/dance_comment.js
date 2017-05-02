89a0379cec4e50cdac1f4499ad963d6f25f34b84
Aniela M Wolkonowski <anielamw@gmail.com>
2017-05-02 18:09:43 -0400

    add error message

--- app/assets/javascripts/dance_comment.js
+++ app/assets/javascripts/dance_comment.js
@@ -1,9 +1,15 @@
 submitClick = (event) => {
   event.preventDefault();
 
+  let errorMessage = () => {
+    alert('Comment cannot be blank.');
+  };
+
+  console.log('hello');
 };
 
 $(document).ready(() => {
+  // How do I use the EventListener `onsubmit` instead of `onclick`? I want the existing required field errors to be the standard.
   $('#dance-show #actions').on('click', submitClick);
   // $('#dance-show #forms').on('submit', submitClick);
 });
