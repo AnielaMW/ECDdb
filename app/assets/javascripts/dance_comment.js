e91f05ae27a97774b8f501a7b4862716becc3084
Aniela M Wolkonowski <anielamw@gmail.com>
2017-05-02 18:56:45 -0400

    extract variable values

--- app/assets/javascripts/dance_comment.js
+++ app/assets/javascripts/dance_comment.js
@@ -9,9 +9,15 @@ submitClick = (event) => {
     alert('Comment cannot be blank.');
   };
 
+  let tId = $('#dance_comment_comment_type_id').val();
+  let com = $('#dance_comment_comment').val();
+  // How do I extract the value of the next two variables as right now all I get is the element?
+  let uId = $('#user-id');
+  let dId = $('#dance-id');
+
   let newCom = new NewComment();
 
-  console.log('hello');
+  console.log(`${tId}, ${com}, ${uId}, ${dId}`);
 };
 
 $(document).ready(() => {
