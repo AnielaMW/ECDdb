f82b1d65db94bd1b41dd0617791c1ec2b05a9652
Aniela M Wolkonowski <anielamw@gmail.com>
2017-05-02 17:50:51 -0400

    add prevent default event function

--- app/assets/javascripts/dance_comment.js
+++ app/assets/javascripts/dance_comment.js
@@ -1,3 +1,8 @@
+submitClick = (event) => {
+  event.preventDefault();
+
+};
+
 $(document).ready(() => {
   $('#dance-show #actions').on('click', submitClick);
   // $('#dance-show #forms').on('submit', submitClick);
