4328346e21487ccf2ef89b58115987081a5ccf79
Aniela M Wolkonowski <anielamw@gmail.com>
2017-05-03 13:48:30 -0400

    start AJAX post and get request functions

--- app/assets/javascripts/dance_comment.js
+++ app/assets/javascripts/dance_comment.js
@@ -11,7 +11,6 @@ submitClick = (event) => {
 
   let tId = $('#dance_comment_comment_type_id').val();
   let com = $('#dance_comment_comment').val();
-  // How do I extract the value of the next two variables as right now all I get is the element?
   let uId = $('#user-id').text();
   let dId = $('#dance-id').text();
 
@@ -19,7 +18,9 @@ submitClick = (event) => {
     // AJAX post request
   };
 
-  let newCom = new NewComment();
+  let newCom = new NewComment(
+    // AJAX get request
+  );
 
   console.log(`${tId}, ${com}, ${uId}, ${dId}`);
 };
