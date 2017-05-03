a5a8a002e11d91bbe3d96b05adb57986bf3a9188
Aniela M Wolkonowski <anielamw@gmail.com>
2017-05-03 15:30:25 -0400

    add on submit event listener(s)

--- app/assets/javascripts/dance_comment.js
+++ app/assets/javascripts/dance_comment.js
@@ -23,10 +23,9 @@ submitClick = (event) => {
   );
 
   console.log(`${tId}, ${com}, ${uId}, ${dId}`);
+  console.log();
 };
 
 $(document).ready(() => {
-  // How do I use the EventListener `onsubmit` instead of `onclick`? I want the existing required field errors to be the standard.
-  $('#dance-show #actions').on('click', submitClick);
-  // $('#dance-show #forms').on('submit', submitClick);
+  $('#dance-show #dc-form').on('submit', submitClick);
 });
