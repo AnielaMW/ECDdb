0a0163b7511c8f28c6b1cbcdb47666a5ee033d1e
Aniela M Wolkonowski <anielamw@gmail.com>
2017-05-19 11:37:01 -0400

    remove description row until called for

--- app/assets/javascripts/dance_directions/add.js
+++ app/assets/javascripts/dance_directions/add.js
@@ -33,4 +33,5 @@ addBlankDirection = () => {
 
 $(document).ready(() => {
   $('#dance-new').on('click', '#dd-add', addBlankDirection);
+  $('#dance-edit').on('click', '#dd-add', addBlankDirection);
 });
