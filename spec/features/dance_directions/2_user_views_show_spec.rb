9f8236b3b8ca823ddec0de75074644d7dc594db1
Aniela M Wolkonowski <anielamw@gmail.com>
2017-05-23 23:05:05 -0400

    trying to make hound happy

--- spec/features/dance_directions/2_user_views_show_spec.rb
+++ spec/features/dance_directions/2_user_views_show_spec.rb
@@ -11,7 +11,7 @@ feature 'view dance_directions#show', %{
   # When a guest clicks on a link from the dance_path,
   # they should expect to see all details of a dance_direction
   # Details must include Dance.title, sequence, direction
-  # Details may include mark, measure, position, (possibly in  the future... Comments)
+  # Details may include mark, measure, position, comments
 
   let!(:dir1) { FactoryGirl.create(:dd) }
   let!(:dir2) { FactoryGirl.create(:dod) }
@@ -24,7 +24,7 @@ feature 'view dance_directions#show', %{
     expect(page).to have_content(dir1.sequence)
   end
 
-  scenario 'sucessfully view a dance_direction#show with optional information' do
+  scenario 'sucessfully view dance_direction#show with optional information' do
     visit dance_dance_direction_path(dir2.dance_id, dir2.id)
 
     expect(page).to have_content(dir2.direction)
@@ -37,9 +37,12 @@ feature 'view dance_directions#show', %{
 
   scenario 'sucessfully view a dance_directions#show from dance_path' do
     visit dance_path(dir1.dance_id)
-    # May eventutally want to be able to click anywhere within this sequences table row to access this page.
+    # May eventutally want to be able to click anywhere within this sequences
+    # table row to access this page.
     click_link dir1.sequence.to_s
 
-    expect(page).to have_current_path(dance_dance_direction_path(dir1.dance_id, dir1.id))
+    expect(page).to have_current_path(
+      dance_dance_direction_path(dir1.dance_id, dir1.id)
+    )
   end
 end
