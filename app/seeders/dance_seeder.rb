3d5935216d67f0bd43649ef837db7bb62c04452a
Aniela M Wolkonowski <anielamw@gmail.com>
2017-05-30 17:40:53 -0400

    trying to make hound happy

--- app/seeders/dance_seeder.rb
+++ app/seeders/dance_seeder.rb
@@ -1,11 +1,11 @@
 class DanceDevSeeder
-
-  json = ActiveSupport::JSON.decode(File.read('dance_data/header.json'))
-
+  dance_list = ActiveSupport::JSON.decode(File.read('dance_data/header.json'))
+  dd_list = ActiveSupport::JSON.decode(File.read('dance_data/instruction.json'))
 
   def self.seed!
     # need to create a record for each dance
-    # need to link to, via foreign key, or create a new record for each formation, key*, level*, meter, mood*, and tempo*
+    # need to link to, via foreign key, or create a new record for each
+    # formation, key*, level*, meter, mood*, and tempo*
     # need to seed each dance_direction with it's associated dance
     # associate all of these seeded dances with the user Dan Seppler
   end
