02df9fa61a43664edfa6ed87cccde5df7e37101f
Aniela M Wolkonowski <anielamw@gmail.com>
2017-06-05 16:40:33 -0400

    add comment types seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,18 @@
 require_relative 'seeders/user_seeder'
 require_relative 'seeders/dances_seeder'
 require_relative 'seeders/dance_directions_seeder'
+require_relative 'seeders/comment_types_seeder'
 
 if Rails.env.development?
   UserDevSeeder.seed!
   DanceDevSeeder.seed!
   DirectionDevSeeder.seed!
+  CommentTypeDevSeeder.seed!
 end
 
 if Rails.env.production?
   UserDevSeeder.seed!
   DanceDevSeeder.seed!
   DirectionDevSeeder.seed!
+  CommentTypeDevSeeder.seed!
 end
